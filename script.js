8b0fd78cfd3f937b5030c894a532e17bed8d9eba
jin <141108687+rffkive@users.noreply.github.com>
2025-07-25 11:46:18 +0800

    declare the first subscibe button in main page
    check by print the output in console log

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 // click main button, go to success card.show and go to main.card.hide
 //click subsribce, vice versa/
 // check transition
+
+const subsBtn = document.querySelector('.subscribe-button');
+
+subsBtn.addEventListener ('click', () => 
+ {
+    console.log('share button 1');
+ }
+);
